630f2b6296f73b628b618ff459c75e45bbb20c9a
RokuJS <shiuxcode@gmail.com>
2026-01-08 13:08:49 -0500

    feat: Set up FastAPI application with routers for categories and spare parts

---
+++ app/routers/__init__.py
@@ -0,0 +1,7 @@
+from routers.categories import router as categories_router
+from routers.spare_parts import router as spare_parts_router
+
+__all__ = [
+    "categories_router",
+    "spare_parts_router",
+]
