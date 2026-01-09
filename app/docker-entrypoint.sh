20fdafca8e358de13fdd93c6993f1b0aa428aa5e
RokuJS <shiuxcode@gmail.com>
2026-01-08 23:38:17 -0500

    feat: Update Dockerfile and entrypoint script to set the correct ENTRYPOINT and apply database migrations

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 alias uv="/root/.local/bin/uv"
 
+# apply database migrations
+echo "Applying database migrations..."
+uv run alembic upgrade head
+
 # run server
 echo "Starting FastAPI server..."
 uv run fastapi dev main.py --port 8000 --host 0.0.0.0
