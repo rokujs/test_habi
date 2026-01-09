5f40d24a701de1eba2a6862888d5b7abbf5e7d63
RokuJS <shiuxcode@gmail.com>
2026-01-08 19:27:39 -0500

    feat: Implement service order creation endpoint with idempotency check

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,7 +1,9 @@
 from routers.categories_api_view import router as categories_router
 from routers.spare_parts_api_views import router as spare_parts_router
+from routers.orders_api_view import router as orders_router
 
 __all__ = [
     "categories_router",
     "spare_parts_router",
+    "orders_router",
 ]
